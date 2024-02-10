2282fb13e5aab2d398023127ffe3fbf0a0643859
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-09 23:56:42 -0600

    Update main.dart
    creando el drawer con sus respectivas funcionalidades

--- lib/main.dart
+++ lib/main.dart
@@ -30,3 +30,48 @@ class MainMenu extends StatelessWidget {
       body: Center(
         child: Text('Bienvenidos al menu principal de la aplicaicion para CEUTEC'),
       ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu de Opciones'),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.article),
+              title: Text('Espacio para Noticias'),
+              onTap: () {
+                Navigator.pushNamed(context, '/noticias');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.task),
+              title: Text('Lista de Tareas'),
+              onTap: () {
+                Navigator.pushNamed(context, '/tareas');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.attach_money),
+              title: Text('Cambio de Monedas'),
+              onTap: () {
+                Navigator.pushNamed(context, '/monedas');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.headset),
+              title: Text('Podcasts'),
+              onTap: () {
+                Navigator.pushNamed(context, '/podcasts');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
